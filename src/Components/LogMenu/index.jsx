3f02a877c9a54c11158f5bd5dcabd361954c050f
Douglas Camargo <camargo.douglas@icloud.com>
2021-06-09 00:51:23 -0300

    adding LogMenu and MessageMenu, making UploadMenu functional and fixing up some css bugs

--- src/Components/LogMenu/index.jsx
+++ src/Components/LogMenu/index.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import { FiLoader, FiCheckCircle } from "react-icons/fi";
 
 import './styles.css'
 export default class LogMenu extends Component {
@@ -7,6 +8,25 @@ export default class LogMenu extends Component {
         return (
             <div className="sub-menu">
                 <h2>3 - Log de Envios</h2>
+
+                <div className="log-list-box">
+                    <h3>Error!</h3>
+                    <ul>
+                        <li className="log-client-box"><div className="log-client-info log-client-name">Pelé</div><div className="log-client-info log-client-telephone">+559123456789</div><div className="log-client-info log-client-status error">ERROR</div></li>
+                        <li className="log-client-box"><div className="log-client-info log-client-name">Pelé</div><div className="log-client-info log-client-telephone">+559123456789</div><div className="log-client-info log-client-status error">ERROR</div></li>
+                    </ul>
+                    <FiLoader className="icon-spin" size={24} style={{marginTop: 16}}/>
+                    <FiCheckCircle color="1eb53a" size={24} style={{marginTop: 16}}/>
+                </div>
+                <div className="log-list-box">
+                    <h3>Éxito!</h3>
+                    <ul>
+                        <li className="log-client-box"><div className="log-client-info log-client-name">Pelé</div><div className="log-client-info log-client-telephone">+559123456789</div><div className="log-client-info log-client-status ok">OK</div></li>
+                        <li className="log-client-box"><div className="log-client-info log-client-name">Pelé</div><div className="log-client-info log-client-telephone">+559123456789</div><div className="log-client-info log-client-status ok">OK</div></li>
+                    </ul>
+                    <FiLoader className="icon-spin" size={24} style={{marginTop: 16}}/>
+                    <FiCheckCircle color="1eb53a" size={24} style={{marginTop: 16}}/>
+                </div>
             </div>
         );
     };
--- src/Components/UploadArchiveMenu/index.jsx
+++ src/Components/UploadArchiveMenu/index.jsx
@@ -1,20 +1,57 @@
 import Upload from '../Upload/index.jsx';
 import { FiHelpCircle, FiPlusCircle, FiTrash2, FiCheckCircle } from 'react-icons/fi';
-import React, { Component } from 'react'
+import React, { Component,FormEvent } from 'react'
 import { Link } from 'react-router-dom';
+import csv from 'csvtojson';
 
 import './styles.css'
 export default class UploadArchiveMenu extends Component {
-    
-    handleUpload = files => {
-        return
+    state = { 
+        clients: [],
+        addSingle: false,
+        inputName: '',
+        inputNumber: '',
     };
 
-    handleAddContact = event => {
+    handleUpload = ([file]) => {
+        let reader = new FileReader();
+        reader.onload = e => csv().fromString(e.target.result).then(jsonArray => this.setState({clients: jsonArray}));
+        reader.readAsText(file);
+    };  
 
+    handleAddContact = event => {
+        console.log(this.state);
+        this.setState({addSingle: true});
+    };
+    
+    handleRevokeAddContact = event => {
+        console.log(this.state);
+
+        this.setState({addSingle: false});
+    };
+    
+    handleConfirmAddContact = (event: FormEvent) => {
+        event.preventDefault();
+        event.stopPropagation();
+        this.setState({
+            clients: this.state.clients.concat({name: this.state.inputName, number: this.state.inputNumber})
+        });
+        this.setState({
+            inputName: '',
+            inputNumber: '',
+            addSingle: false
+        });
+    };
+
+    componentDidMount() {        
+        this.setState({
+            clients: [],
+            addSingle: false,
+        });
     };
     
     render() {
+        const { clients, addSingle } = this.state
         return (
             <div className="sub-menu">
                 <h2>
@@ -24,53 +61,43 @@ export default class UploadArchiveMenu extends Component {
                     </Link>
                 </h2>
                 <Upload placeHolder="Click acá para cargar el archivo..." onUpload={this.handleUpload} />
-                <div className="list-box">
+                <div className="upload-list-box">
                     <ul>
-                        <li className="client-box"><div className="client-info client-name">Pelé</div><div className="client-info client-telephone">+559123456789</div></li>
-                        <li className="client-box"><div className="client-info client-name">Pelé</div><div className="client-info client-telephone">+559123456789</div></li>
-                        <li className="client-box"><div className="client-info client-name">Pelé</div><div className="client-info client-telephone">+559123456789</div></li>
-                        <li className="client-box"><div className="client-info client-name">Pelé</div><div className="client-info client-telephone">+559123456789</div></li>
-                        <li className="client-box"><div className="client-info client-name">Pelé</div><div className="client-info client-telephone">+559123456789</div></li>
-                        <li className="client-box"><div className="client-info client-name">Pelé</div><div className="client-info client-telephone">+559123456789</div></li>
-                        <li className="client-box"><div className="client-info client-name">Pelé</div><div className="client-info client-telephone">+559123456789</div></li>
-                        <li className="client-box"><div className="client-info client-name">Pelé</div><div className="client-info client-telephone">+559123456789</div></li>
-                        <li className="client-box"><div className="client-info client-name">Pelé</div><div className="client-info client-telephone">+559123456789</div></li>
-                        <li className="client-box"><div className="client-info client-name">Pelé</div><div className="client-info client-telephone">+559123456789</div></li>
-                        <li className="client-box"><div className="client-info client-name">Pelé</div><div className="client-info client-telephone">+559123456789</div></li>
-                        <li className="client-box"><div className="client-info client-name">Pelé</div><div className="client-info client-telephone">+559123456789</div></li>
-                        <li className="client-box"><div className="client-info client-name">Pelé</div><div className="client-info client-telephone">+559123456789</div></li>
-                        <li className="client-box"><div className="client-info client-name">Pelé</div><div className="client-info client-telephone">+559123456789</div></li>
-                        <li className="client-box"><div className="client-info client-name">Pelé</div><div className="client-info client-telephone">+559123456789</div></li>
-                        <li className="client-box"><div className="client-info client-name">Pelé</div><div className="client-info client-telephone">+559123456789</div></li>
-                        <li className="client-box"><div className="client-info client-name">Pelé</div><div className="client-info client-telephone">+559123456789</div></li>
-                        <li className="client-box"><div className="client-info client-name">Pelé</div><div className="client-info client-telephone">+559123456789</div></li>
-                        <li className="client-box"><div className="client-info client-name">Pelé</div><div className="client-info client-telephone">+559123456789</div></li>
-                        <li className="client-box"><div className="client-info client-name">Pelé</div><div className="client-info client-telephone">+559123456789</div></li>
-                        <li className="client-box"><div className="client-info client-name">Pelé</div><div className="client-info client-telephone">+559123456789</div></li>
-                        <li className="client-box"><div className="client-info client-name">Pelé</div><div className="client-info client-telephone">+559123456789</div></li>
-                        <li className="client-box"><div className="client-info client-name">Pelé</div><div className="client-info client-telephone">+559123456789</div></li>
-                        <li className="client-box"><div className="client-info client-name">Pelé</div><div className="client-info client-telephone">+559123456789</div></li>
-                        <li className="client-box"><div className="client-info client-name">Pelé</div><div className="client-info client-telephone">+559123456789</div></li>
-                        <li className="client-box"><div className="client-info client-name">Pelé</div><div className="client-info client-telephone">+559123456789</div></li>
-                        <li className="client-box"><div className="client-info client-name">Pelé</div><div className="client-info client-telephone">+559123456789</div></li>
+                        {
+                            clients.map((client, index) => (
+                                    <li key={index} className="upload-client-box">
+                                        <div className="upload-client-info upload-client-name">
+                                            {client.name}
+                                        </div>
+                                        <div className="upload-client-info upload-client-telephone">
+                                            {client.number}
+                                        </div>
+                                    </li>
+                            ))
+                        }
                     </ul>
-                    <button id="addContact" onClick={this.handleAddContact}>
-                        <FiPlusCircle size={24}/>
-                    </button>
-                    <form className="client-box">
-                        <input type="text" className="client-info client-name" />
-                        <input type="text" className="client-info client-telephone" />
-                    </form>
-                    <div className="formButtonWrap">
-                        <button id="revokeAddContact" onClick={this.ha}>
-                            <FiTrash2 size={24}/>
+                    {addSingle ? (
+                        <>
+                            <form id="add-client-form" className="upload-client-box">
+                                <input value={this.state.inputName} onChange={e => this.setState({inputName: e.target.value})} type="text" className="upload-client-info upload-client-name" />
+                                <input value={this.state.inputNumber} onChange={e => this.setState({inputNumber: e.target.value})} type="text" className="upload-client-info client-telephone" />
+                            </form>
+                            <div className="upload-btn-wrap">
+                                <button id="upload-btn-revoke" onClick={this.handleRevokeAddContact}>
+                                    <FiTrash2 size={24}/>
+                                </button>
+                                <button  onClick={this.handleConfirmAddContact} id="upload-btn-confirm">
+                                    <FiCheckCircle size={24}/>
+                                </button>
+                            </div>
+                        </>
+                    ):(
+                        <button id="upload-add-contact" onClick={this.handleAddContact}>
+                            <FiPlusCircle size={24}/>
                         </button>
-                        <button id="confirmAddContact" onClick={this.ha}>
-                            <FiCheckCircle size={24}/>
-                        </button>
-                    </div>
+                    )}
                 </div>
             </div>
-        )
+        );
     }
 }
